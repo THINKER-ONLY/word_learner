81f8b7496c423ac559f2c5dd3077afac34a4a912
THINKER-ONLY <3193304954@qq.com>
2025-06-30 11:06:39 +0800

    [feat]更新了核心代码框架

--- src/core/settings_manager.py
+++ src/core/settings_manager.py
@@ -1,1 +1,22 @@
- 
+import json
+
+class SettingsManager:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.defaults = {
+            "display_interval": 3,
+            "display_mode": "random",
+            "show_chinese": True,
+        }
+
+    def load_settings(self):
+        pass
+
+    def get_settings(self, key):
+        pass
+
+    def set_settings(self, key, value):
+        pass
+
+    def _save_to_file(self):
+        pass
--- src/core/word_manager.py
+++ src/core/word_manager.py
@@ -1,1 +1,39 @@
- 
+import json
+import random
+
+class WordManager:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.words = []
+        self.current_index = -1
+        self._load_words()
+
+    def _load_words(self):
+        pass
+
+    def _save_words(self):
+        pass
+
+    def find_word(self, key, value):
+        pass
+    
+    def get_random_word(self):
+        pass
+
+    def get_next_word(self):
+        pass
+
+    def reset_sequential_index(self):
+        pass
+
+    def get_all_word(self):
+        pass
+
+    def add_word(self, en, cn, mean):
+        pass
+
+    def edit_word(self, origin, new_en, new_cn):
+        pass
+
+    def delete_word(self, key):
+        pass
